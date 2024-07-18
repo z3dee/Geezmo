c2a343bdcbf23f855041909915949dffdb9046af
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-07-18 14:33:39 +0500

    Initial Commit

---
+++ Geezmo Watch App/GeezmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeezmoApp.swift
+//  Geezmo Watch App
+//
+//  Created by Ярослав Седышев on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Geezmo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Geezmo/GeezmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeezmoApp.swift
+//  Geezmo
+//
+//  Created by Ярослав Седышев on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeezmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
