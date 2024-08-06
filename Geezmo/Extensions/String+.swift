fc5df0493f9b21547b0409c6d85ba6d8051f6445
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-08-06 11:21:57 +0500

    Added WakeOnLANService.

---
+++ Geezmo/Extensions/String+.swift
@@ -0,0 +1,21 @@
+//
+//  String+.swift
+//  Geezmo
+//
+//  Created by Ярослав Седышев on 06.08.2024.
+//
+
+import Foundation
+
+extension String {
+    var hexadecimal: Data? {
+        var data = Data(capacity: count / 2)
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSRange(startIndex..., in: self)) { match, _, _ in
+            let byteString = (self as NSString).substring(with: match!.range)
+            let num = UInt8(byteString, radix: 16)!
+            data.append(num)
+        }
+        return data.count > 0 ? data : nil
+    }
+}
---
+++ Geezmo/Services/WakeOnLANService.swift
@@ -0,0 +1,65 @@
+//
+//  WakeOnLANService.swift.swift
+//  Geezmo
+//
+//  Created by Ярослав Седышев on 06.08.2024.
+//
+
+import Foundation
+import Network
+
+class WakeOnLANService {
+    static let shared = WakeOnLANService()
+    
+    private init() {}
+    
+    private func createMagicPacket(macAddress: String) -> Data? {
+        let cleanedMac = macAddress.replacingOccurrences(of: ":", with: "").replacingOccurrences(of: "-", with: "")
+        guard cleanedMac.count == 12, let macData = cleanedMac.hexadecimal else {
+            print("Invalid MAC address")
+            return nil
+        }
+        
+        var magicPacket = Data(repeating: 0xFF, count: 6)
+        for _ in 0..<16 {
+            magicPacket.append(macData)
+        }
+        
+        return magicPacket
+    }
+    
+    // Function to send the Magic Packet
+    func wakeDevice(at ipAddress: String, macAddress: String, port: UInt16 = 9, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let magicPacket = createMagicPacket(macAddress: macAddress) else {
+            completion(.failure(WakeOnLanError.invalidMacAddress))
+            return
+        }
+        
+        let connection = NWConnection(host: NWEndpoint.Host(ipAddress), port: NWEndpoint.Port(rawValue: port)!, using: .udp)
+        
+        connection.stateUpdateHandler = { state in
+            switch state {
+            case .ready:
+                connection.send(content: magicPacket, completion: .contentProcessed { error in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        completion(.success(()))
+                    }
+                    connection.cancel()
+                })
+            case .failed(let error):
+                completion(.failure(error))
+            default:
+                break
+            }
+        }
+        
+        connection.start(queue: .global())
+    }
+    
+    enum WakeOnLanError: Error {
+        case invalidMacAddress
+    }
+}
+
