67d04682b801bb7e79c17cc54892882eac7c2927
Yaroslav Sedyshev <jareksedy@icloud.com>
2024-08-23 16:59:22 +0500

    Machine-translated localization.

--- Geezmo/Resources/Strings.swift
+++ Geezmo/Resources/Strings.swift
@@ -9,112 +9,101 @@
 
 enum Strings {
     enum Titles {
-        static let apps = "Apps"
-        static let preferences = "Preferences"
-        static let aboutApp = "About Geezmo"
-        static let faq = "Frequently Asked Questions"
-        static let rateUs = "Rate Us On App Store"
-        static let connectTV = "Connect TV"
-        static let manuallyEnterIP = "Manually Input IP Address"
-        static let resetConnectionData = "Reset Connection Data"
-
-        static let alternativeLayout = "Alternative Layout"
-        static let hapticFeedback = "Haptic Feedback"
-        
-        static let pairingTitle = "Pairing Code"
+        static let apps = "#apps".localized
+        static let preferences = "#preferences".localized
+        static let aboutApp = "#aboutApp".localized
+        static let faq = "#faq".localized
+        static let rateUs = "#rateUs".localized
+        static let connectTV = "#connectTV".localized
+        static let manuallyEnterIP = "#manuallyEnterIP".localized
+        static let resetConnectionData = "#resetConnectionData".localized
+        static let alternativeLayout = "#alternativeLayout".localized
+        static let hapticFeedback = "#hapticFeedback".localized
+        static let pairingTitle = "#pairingTitle".localized
     }
 
     enum SectionHeaders {
-        static let app = "App"
-        static let connection = "Connection"
-        static let layoutAndHaptics = "Layout and haptics"
+        static let app = "#app".localized
+        static let connection = "#connection".localized
+        static let layoutAndHaptics = "#layoutAndHaptics".localized
     }
 
     enum About {
-        static let appInfo = "LG Smart TV Remote Control App"
-        static let authorInfo = "by Yaroslav Sedyshev"
-        static let madeWith = "Made with"
-        static let madeIn = "in Kazakhstan"
+        static let appInfo = "#appInfo".localized
+        static let authorInfo = "#authorInfo".localized
+        static let madeWith = "#madeWith".localized
+        static let madeIn = "#madeIn".localized
     }
     
     enum Apps {
-        static let loadingApps = "Please wait while we load your TV app list"
-        static let noAppsFound = "Something went wrong while loading your TV app list. Please Reload."
-        static let reload = "Reload App List"
+        static let loadingApps = "#loadingApps".localized
+        static let noAppsFound = "#noAppsFound".localized
+        static let reload = "#reload".localized
     }
 
     enum ConnectTV {
-        static let discoveredDevices = "Discovered devices"
-        static let importantNote = "TV must be on and connected to the same network"
-        static let notFound = "No TV was discovered. Please rescan your network."
-        static let rescan = "Rescan Network"
+        static let discoveredDevices = "#discoveredDevices".localized
+        static let importantNote = "#importantNote".localized
+        static let notFound = "#notFound".localized
+        static let rescan = "#rescan".localized
     }
 
     enum InputIP {
-        static let inputIPMessage = "IP address of your TV"
-        static let inputIPPrompt = "IP address"
+        static let inputIPMessage = "#inputIPMessage".localized
+        static let inputIPPrompt = "#inputIPPrompt".localized
     }
 
     enum ToastMessages {
-        static let prompted = "Please accept the registration prompt on the TV"
-        static let connectedAndRegistered = "Successfully connected and registered with the TV"
-        static let disconnected = "Disconnected, tap any button to reconnect"
-        static let tvTurnedOn = "TV has been successfully powered on and is ready to use"
-        static let tvGoingOff = "TV is going off, tap the power button to turn it back on"
+        static let prompted = "#prompted".localized
+        static let connectedAndRegistered = "#connectedAndRegistered".localized
+        static let disconnected = "#disconnected".localized
+        static let tvTurnedOn = "#tvTurnedOn".localized
+        static let tvGoingOff = "#tvGoingOff".localized
     }
 
     enum FAQ {
-        static let q1 = "Does it come with a companion Apple Watch app?"
-        static let a1 = "Yes, Geezmo comes with a fantastic companion app specifically designed for the Apple Watch. Check it out!"
-        
-        static let q2 = "Can I customize the button layout?"
-        static let a2 = "Currently, customization options are somewhat limited. Geezmo offers a basic layout and an alternative layout, which you can switch between in the settings menu."
-        
-        static let q3 = "Can I hold a button for a continuous action?"
-        static let a3 = "Yes, certain button types such as volume controls and arrow keys support continuous actions when held down."
-        
-        static let q4 = "How do I troubleshoot connection issues?"
-        static let a4 = "Ensure that both your iPhone and your TV are connected to the same Wi-Fi network and that you have granted local network permissions to Geezmo. This is essential for the app to function properly. Please also ensure that your TV has the latest software installed to guarantee the best experience."
-        
-        static let q5 = "Does Geezmo support voice commands?"
-        static let a5 = "No, currently Geezmo does not support voice commands."
-        
-        static let q6 = "Can I use Geezmo with multiple TVs?"
-        static let a6 = "No, not in the current version. Connecting to a different TV will reset the previously stored connection data."
-        
-        static let q7 = "Are Samsung and other non-LG TVs supported?"
-        static let a7 = "No, and there are no plans to support TVs of other brands. This app is dedicated solely to WebOS TVs, such as those made by LG."
-        
-        static let q8 = "Is Android version available?"
-        static let a8 = "No, currently, Geezmo is only available for iOS devices."
-        
-        static let q9 = "Is Geezmo free and open source?"
-        static let a9 = "Yes, Geezmo is completely free and always will be. Additionally, it is open-source. Please visit our GitHub repository at"
-        
-        static let q10 = "Questions? Comments?"
-        static let a10 = "For any questions regarding this app, feel free to reach out to me at"
+        static let q1 = "#q1".localized
+        static let a1 = "#a1".localized
+        static let q2 = "#q2".localized
+        static let a2 = "#a2".localized
+        static let q3 = "#q3".localized
+        static let a3 = "#a3".localized
+        static let q4 = "#q4".localized
+        static let a4 = "#a4".localized
+        static let q5 = "#q5".localized
+        static let a5 = "#a5".localized
+        static let q6 = "#q6".localized
+        static let a6 = "#a6".localized
+        static let q7 = "#q7".localized
+        static let a7 = "#a7".localized
+        static let q8 = "#q8".localized
+        static let a8 = "#a8".localized
+        static let q9 = "#q9".localized
+        static let a9 = "#a9".localized
+        static let q10 = "#q10".localized
+        static let a10 = "#a10".localized
     }
 
     enum General {
-        static let appName = "Geezmo App"
-        static let shortAppName = "Geezmo"
-        static let save = "Save"
-        static let reset = "Reset"
-        static let cancel = "Cancel"
-        static let yourTextHere = "Enter text here..."
-        static let error = "Error"
+        static let appName = "#appName".localized
+        static let shortAppName = "#shortAppName".localized
+        static let save = "#save".localized
+        static let reset = "#reset".localized
+        static let cancel = "#cancel".localized
+        static let yourTextHere = "#yourTextHere".localized
+        static let error = "#error".localized
     }
     
     enum Alerts {
         enum MulticastPermissionsDenied {
-            static let title = "Permission Denied"
-            static let message = "Multicast networking permissions are required to discover and connect to your TV. Please enable these permissions in Settings → Privacy & Security → Local Network."
-            static let primaryButtonTitle = "Open Settings"
+            static let title = "#multicastPermissionsDenied_title".localized
+            static let message = "#multicastPermissionsDenied_message".localized
+            static let primaryButtonTitle = "#multicastPermissionsDenied_primaryButtonTitle".localized
         }
         
         enum ResetConnectionData {
-            static let title = "Do you want to reset connection data?"
-            static let message = "You will need to reconnect and re-register with the TV."
+            static let title = "#resetConnectionData_title".localized
+            static let message = "#resetConnectionData_message".localized
         }
     }
 }
